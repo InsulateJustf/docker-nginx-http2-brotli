18ec0e47da10d5decfef1ddb27f8dcd38729f5a6
InsulateJustf <justforsomeonen@gmail.com>
2022-07-27 01:37:44 +0800

    fix volumes

--- run-docker.sh
+++ run-docker.sh
@@ -3,11 +3,11 @@ docker run --rm \
   -p 0.0.0.0:8888:80 \
   -p 0.0.0.0:8889:443/tcp \
   -p 0.0.0.0:8889:443/udp \
-  -v "/tmp/nginx/tests":/static:ro \
-  -v "/tmp/nginx/tests/modules.conf":/etc/nginx/main.d/modules.conf:ro \
-  -v "/tmp/nginx/tests/static.conf":/etc/nginx/conf.d/static.conf:ro \
-  -v "/tmp/nginx/tests/https.conf":/etc/nginx/conf.d/https.conf:ro \
-  -v "/tmp/nginx/tests/localhost.crt":/etc/nginx/ssl/localhost.crt:ro \
-  -v "/tmp/nginx/tests/localhost.key":/etc/nginx/ssl/localhost.key:ro \
+  -v "$PWD/tests":/static:ro \
+  -v "$PWD/tests/modules.conf":/etc/nginx/main.d/modules.conf:ro \
+  -v "$PWD/tests/static.conf":/etc/nginx/conf.d/static.conf:ro \
+  -v "$PWD/tests/https.conf":/etc/nginx/conf.d/https.conf:ro \
+  -v "$PWD/tests/localhost.crt":/etc/nginx/ssl/localhost.crt:ro \
+  -v "$PWD/tests/localhost.key":/etc/nginx/ssl/localhost.key:ro \
   --name test_nginx \
   -t justf/nginx-http2-brotli
