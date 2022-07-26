7eb65289aaa4f1ebd3b0a6d780817f697363ef53
InsulateJustf <justforsomeonen@gmail.com>
2022-07-26 18:57:04 +0800

    fix perl error

--- run-docker.sh
+++ run-docker.sh
@@ -5,7 +5,7 @@ docker run --rm \
   -p 0.0.0.0:8889:443/udp \
   -v "$PWD/tests":/static:ro \
   -v "$PWD/tests/modules.conf":/etc/nginx/main.d/modules.conf:ro \
-  -v "$PWD/tests/perl_rewrite.conf":/etc/nginx/conf.d/perl_rewrite.conf:ro \
+  # -v "$PWD/tests/perl_rewrite.conf":/etc/nginx/conf.d/perl_rewrite.conf:ro \
   -v "$PWD/tests/static.conf":/etc/nginx/conf.d/static.conf:ro \
   -v "$PWD/tests/https.conf":/etc/nginx/conf.d/https.conf:ro \
   -v "$PWD/tests/localhost.crt":/etc/nginx/ssl/localhost.crt:ro \
